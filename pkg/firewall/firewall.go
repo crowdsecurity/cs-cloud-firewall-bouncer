c67e4c37f1ab5f064dda3aece711945de4f6d671
Francois Allard <francois.allard@ymail.com>
2020-12-21 10:30:11 -0500

    [Bug fix] Fixing rule generator (#4)

--- pkg/firewall/firewall.go
+++ pkg/firewall/firewall.go
@@ -7,8 +7,8 @@ import (
 	csmodels "github.com/crowdsecurity/crowdsec/pkg/models"
 	"github.com/fallard84/cs-cloud-firewall-bouncer/pkg/models"
 	"github.com/fallard84/cs-cloud-firewall-bouncer/pkg/providers"
+	"github.com/sethvargo/go-diceware/diceware"
 	log "github.com/sirupsen/logrus"
-	"github.com/zhexuany/wordGenerator"
 )
 
 type Bouncer struct {
@@ -115,8 +115,8 @@ func (f *Bouncer) getRuleToUpdate(rules []*models.FirewallRule) (*models.Firewal
 
 func (f *Bouncer) genNewRuleName() string {
 
-	random := wordGenerator.GetWords(5, 20)
-	r := fmt.Sprintf("%s-%s", f.RuleNamePrefix, strings.ToLower(strings.Join(random, "-")))
+	randomWords := diceware.MustGenerate(2)
+	r := fmt.Sprintf("%s-%s", f.RuleNamePrefix, strings.ToLower(strings.Join(randomWords, "-")))
 	return r
 }
 
