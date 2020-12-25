bf07d1e6590560fc8ad234becb4bc007fe3589da
Francois Allard <francois@breathelife.com>
2020-12-25 15:32:48 -0500

    Debug

--- testing/integration/e2e/e2e-coverage.sh
+++ testing/integration/e2e/e2e-coverage.sh
@@ -14,9 +14,7 @@ pushd testing/integration/e2e || exit 1
     echo "Waiting 15 sec, then stopping process"
     sleep 30
     echo "Stopping process"
-    ps -ewo pid,cmd
-    ps -ewo pid,cmd | grep "cs-cloud-firewall-bouncer_instr-bin" |  grep -v "grep" | awk '{ print $1 }'
-    ps -ewo pid,cmd | grep "cs-cloud-firewall-bouncer_instr-bin" |  grep -v "grep" | awk '{ print $1 }' | xargs -I{} kill -n SIGTERM {}
+    ps -ewo pid,cmd | grep "cs-cloud-firewall-bouncer_instr-bin" |  grep -v "grep" | awk '{ print $1 }' | xargs -I{} kill -SIGTERM {}
     echo "Stopping mock server"
     docker-compose down
 popd || exit 1
